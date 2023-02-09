a0da615089e7dd2a565dd04d01587e4f3404cf14
Danny Greer <dannygreer@gmail.com>
2023-02-09 15:31:43 -0600

    Add files via upload

---
+++ JU/ju-da.js
@@ -0,0 +1,13 @@
+<script>
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("page-block-g4bap9b2ec6").style.top = "0";
+  } else {
+    document.getElementById("page-block-g4bap9b2ec6").style.top = "-70px";
+  }
+}
+
+</script>
