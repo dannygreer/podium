c156cedf901bc398cf33f1599fd08627ef8c25c9
Danny Greer <dannygreer@gmail.com>
2023-02-24 11:42:36 -0600

    test

---
+++ test/test-ite.js
@@ -0,0 +1,13 @@
+<script>
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("page-block-nul6wdd9ehj").style.top = "0";
+  } else {
+    document.getElementById("page-block-nul6wdd9ehj").style.top = "-70px";
+  }
+}
+
+</script>
