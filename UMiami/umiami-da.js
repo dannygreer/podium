c50458b36865044807ddf07f5d485449a475d1bf
Danny Greer <dannygreer@gmail.com>
2023-08-02 09:26:11 -0500

    podium 8-23

---
+++ UMiami/umiami-da.js
@@ -0,0 +1,13 @@
+<script>
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("page-block-p1pk70toe7o").style.top = "0";
+  } else {
+    document.getElementById("page-block-p1pk70toe7o").style.top = "-70px";
+  }
+}
+
+</script>
